b9e91d60ae313468c07747f3cc6ab732b51ed3e3
exemas <exequielschvartz@gmail.com>
2020-06-26 18:06:45 -0300

    server

---
+++ server/build/precache-manifest.820c8fdaa80e16876c22548115eb7c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f6b5ce9af682db2be17a720e123425e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "056081c0e8f9ad5a3a5e",
+    "url": "/static/css/main.ecbef360.chunk.css"
+  },
+  {
+    "revision": "ae5f6d38d68cb0a3addd",
+    "url": "/static/js/2.09963126.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.09963126.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056081c0e8f9ad5a3a5e",
+    "url": "/static/js/main.e9f4ea2f.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
