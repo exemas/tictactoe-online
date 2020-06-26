54d92af21ab1da261be7804d0ca6f87da012c32a
exemas <exequielschvartz@gmail.com>
2020-06-26 18:29:07 -0300

    a

--- server/index.js
+++ server/index.js
@@ -98,11 +98,7 @@ io.on("connect", (socket) => {
 });
 
 app.use(cors());
-//app.use(router);
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+app.use(router);
 
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
