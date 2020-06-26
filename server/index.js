0b20b0868a4334b5da885cd180fbec053300298d
exemas <exequielschvartz@gmail.com>
2020-06-26 18:46:28 -0300

    server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
 const router = require("./router");
-
+const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -98,7 +98,11 @@ io.on("connect", (socket) => {
 });
 
 app.use(cors());
-app.use(router);
+//app.use(router);
+app.use(express.static(path.join(__dirname, "build")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 server.listen(process.env.PORT || 5000, () =>
   console.log(`Server has started.`)
