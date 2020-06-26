82eef053c3bebcbb36e62af745f94249161969e5
exemas <exequielschvartz@gmail.com>
2020-06-26 12:00:13 -0300

    corregido footer

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faGithub,
+  faReact,
+  faNodeJs,
+} from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
   return (
     <div
       style={{
         color: "white",
-        height: "100px",
+        height: "150px",
         textAlign: "center",
         textShadow: "1px 1px 3px #333333",
       }}
@@ -15,7 +22,28 @@ function Footer() {
         <span>
           <img src="./img/corazon.png" alt="" height="20px" />
         </span>{" "}
-        por Exequiel Schvartz
+        por Exequiel Schvartz. Powered by{" "}
+        <FontAwesomeIcon icon={faReact} style={{ fontSize: "20px" }} /> &{" "}
+        <FontAwesomeIcon icon={faNodeJs} style={{ fontSize: "20px" }} />.
+      </p>
+      <p>
+        <a
+          href="https://linkedin.com/in/exequielschvartz"
+          target="_blank"
+          style={{ color: "white" }}
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+        {"  "}
+        <a
+          href="https://github.com/exemas/tictactoe-online"
+          target="_blank"
+          style={{ color: "white" }}
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} size="2x" />
+        </a>
       </p>
     </div>
   );
